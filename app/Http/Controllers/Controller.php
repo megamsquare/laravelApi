7a8d49d364aa7d1d854eceaa7f3219f04ba5b9be
Mensah Michael <msquaremega@gmail.com>
2020-07-01 05:32:00 +0100

    OpenAPI annotation implementation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,33 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Laravel Api Documentation",
+     *      description="L5 Swagger OpenApi description",
+     *      @OA\Contact(
+     *          email="msquaremega@gmail.com"
+     *      ),
+     *      @OA\License(
+     *          name="Apache 2.0",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Laravel API Server"
+     * )
+
+     *
+     * @OA\Tag(
+     *     name="Projects",
+     *     description="API Endpoints of Projects"
+     * )
+     */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
