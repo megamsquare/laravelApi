f57f70c1c737ecd60b61a660109f617c3e96f7b1
Mensah Michael <msquaremega@gmail.com>
2020-07-03 06:33:13 +0100

    add table in country model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     /**
+     * The table that is associated with the model
+    */
+    protected $table = 'countries';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
