fad66fb9d55834f766f3529fdedbc8035f45fc1b
Mensah Michael <msquaremega@gmail.com>
2020-07-04 01:38:13 +0100

    working on update user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -109,6 +109,17 @@ class AuthController extends Controller
         }
     }
 
+    public function updateUser(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        if ($user) {
+            # code...
+        } else {
+            return response()->json(['error' => 'Could not update your details']);
+        }
+    }
+
     /**
      * Get the authenticated User
      *
