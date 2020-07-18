8b2d6c6617d65a36210792be879698138b6a1333
Mensah Michael <msquaremega@gmail.com>
2020-07-18 19:31:27 +0100

    add functions to country controller

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class CountryController extends Controller
 {
     //
+
+    public function index() {}
+
+    public function create() {}
+
+    public function update() {}
+
+    public function delete() {}
 }
