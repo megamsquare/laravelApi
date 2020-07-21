3a5cafc6ad502f568246ae6a62428fc8c5c6c439
Mensah Michael <msquaremega@gmail.com>
2020-07-21 23:40:55 +0100

    crud functions created in grade controller

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class GradeController extends Controller
 {
     //
+    public function index() {}
+
+    public function create() {}
+
+    public function edit() {}
+
+    public function delete() {}
 }
