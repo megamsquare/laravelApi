e9262b5c908e68d46017bba59bbd8f1499151b26
Mensah Michael <msquaremega@gmail.com>
2020-08-01 15:09:46 +0100

    get all company

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Country;
 
 class CompanyController extends Controller
 {
     //
     public function index() {
         // Return all Companies regardless of user
+        $country = Country::orderby('created_at')->get();
+        return response()->json(['country' => $country]);
     }
 
     public function create() {}
