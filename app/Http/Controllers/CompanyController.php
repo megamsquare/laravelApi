41fc18603acc972a75a9ad4c042d383502468ba4
Mensah Michael <msquaremega@gmail.com>
2020-08-01 15:03:24 +0100

    add controller to country controller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CompanyController extends Controller
 {
     //
-    public function index() {}
+    public function index() {
+        // Return all Companies regardless of user
+    }
 
     public function create() {}
 
