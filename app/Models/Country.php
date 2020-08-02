222110bea33a7fc1142fca36243b8222b0591a7c
Mensah Michael <msquaremega@gmail.com>
2020-08-03 00:08:14 +0100

    use soft delete in country model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Country extends Model
 {
@@ -10,6 +11,12 @@ class Country extends Model
      * The table that is associated with the model
     */
     protected $table = 'countries';
+
+    /**
+     * Using Soft Deletes for the model
+     */
+    use SoftDeletes;
+
 
     /**
      * The attributes that are mass assignable.
